a405fd4fd5e7388fd2926812234fac92300c0b41
shirishakodari <kodarishirisha419@gmail.com>
2025-08-30 17:15:42 +0530

    jenkins

--- vars/sampleEKSPipeline.groovy
+++ vars/sampleEKSPipeline.groovy
@@ -1,3 +1,65 @@
 def call(Map configMap){
+     pipeline {
+     // agent any 
+        agent {
+            label 'AGENT-1'
+        }
+        environment {
+            COURSE = 'jenkins'
 
-}
+        }
+        options { // pipeline expries 30 mint
+            timeout(time: 30, unit: 'MINUTES')
+            disableConcurrentBuilds() // not parallel to pipelines at a time so, one complted after another complted
+        }
+        parameters {
+            string(name: 'PERSON', defaultValue: 'Mr Jenkins', description: 'Who should I say hello to?')
+            text(name: 'BIOGRAPHY', defaultValue: '', description: 'Enter some information about the person')
+            booleanParam(name: 'TOGGLE', defaultValue: true, description: 'Toggle this value')
+            choice(name: 'CHOICE', choices: ['One', 'Two', 'Three'], description: 'Pick something')
+            password(name: 'PASSWORD', defaultValue: 'SECRET', description: 'Enter a password')
+        }
+    // build
+        stages {
+            stage('Build') {
+                steps {
+                    script{
+                    // echo 'Building..'
+                    sh """
+                    echo "hello world"
+                    sleep 10
+                    env
+                    echo "hello ${params.PERSON}"
+                    """
+                    }
+                    
+                } 
+            }
+            stage('Test') {
+                steps {
+                    script{
+                        echo 'Testing..'
+                    }
+                    
+                }
+            }
+            
+        }
+            
+        post { 
+            always { 
+                echo 'I will always say Hello again!'
+                deleteDir() // delete post build pipeline in workspace  
+            }
+            success { 
+                echo 'hello success'
+            }
+            failure { 
+                echo 'hello failure'
+            }
+        }
+
+    }
+
+
+    }
